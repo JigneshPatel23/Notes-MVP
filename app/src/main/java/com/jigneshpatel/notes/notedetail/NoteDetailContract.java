bdfcd2cbb9603a8b32dc3a0d9dd27c0488f27014
Jignesh Patel <jignesh.patel@solutelabs.com>
2016-02-08 17:03:35 +0530

    contract design for view and user actions

--- app/src/main/java/com/jigneshpatel/notes/notedetail/NoteDetailContract.java
+++ app/src/main/java/com/jigneshpatel/notes/notedetail/NoteDetailContract.java
@@ -1,7 +1,24 @@
 package com.jigneshpatel.notes.notedetail;
 
+import android.support.annotation.Nullable;
+
 /**
  * Created on 8/2/16.
  */
-public class NoteDetailContract {
+public interface NoteDetailContract {
+
+    interface View{
+        void setProgressIndicator(boolean active);
+        void showMissingNoteUi();
+        void showTitle(String title);
+        void hideTitle();
+        void showDescription(String description);
+        void hideDescription();
+        void showImage(String imageUrl);
+        void hideImage();
+    }
+
+    interface UserActionListener{
+        void openNote(@Nullable String noteId);
+    }
 }
--- app/src/main/java/com/jigneshpatel/notes/notes/NotesContract.java
+++ app/src/main/java/com/jigneshpatel/notes/notes/NotesContract.java
@@ -1,7 +1,26 @@
 package com.jigneshpatel.notes.notes;
 
+import android.support.annotation.NonNull;
+
+import com.jigneshpatel.notes.data.Note;
+
+import java.util.List;
+
 /**
  * Created on 8/2/16.
  */
 public interface NotesContract {
+
+    interface View{
+        void setProgressIndicator(boolean active);
+        void showNotes(List<Note> notes);
+        void showAddNoteUi();
+        void showNoteDetailUi(String noteId);
+    }
+
+    interface UserActionListener{
+        void loadNotes(boolean forceUpdate);
+        void addNewNote();
+        void openNoteDetails(@NonNull Note note);
+    }
 }
