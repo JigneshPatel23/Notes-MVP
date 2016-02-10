235729f39fc7bf96b5006a49da21cad62aac82ca
Jignesh Patel <jignesh.patel@solutelabs.com>
2016-02-10 19:25:17 +0530

    design navigation based skeleton of application - activity / fragment skeleton - layout designs - bind layouts to activity / fragment - add simple navigation actions to go through screens

---
+++ app/src/main/java/com/jigneshpatel/notes/notedetail/NoteDetailFragment.java
@@ -0,0 +1,31 @@
+package com.jigneshpatel.notes.notedetail;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jigneshpatel.notes.R;
+
+/**
+ * Created on 10/2/16.
+ */
+public class NoteDetailFragment extends Fragment {
+
+    public static final String ARGUMENT_NOTE_ID = "NOTE_ID";
+
+    public static NoteDetailFragment newInstance(String noteId) {
+        Bundle arguments = new Bundle();
+        arguments.putString(ARGUMENT_NOTE_ID, noteId);
+        NoteDetailFragment fragment = new NoteDetailFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+}
